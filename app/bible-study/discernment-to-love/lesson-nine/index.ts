16dce623c37b6ff6e89d8591d45a17ae884d3022
komed94-jpg <komed94@gmail.com>
2026-07-28 18:02:42 +0900

    9과 페이지 데이터 연결

---
+++ app/bible-study/discernment-to-love/lesson-nine/index.ts
@@ -0,0 +1,19 @@
+import { lessonNineOpening } from "./opening";
+import { lessonNinePartOne } from "./part_1";
+import { lessonNinePartTwo } from "./part_2";
+import { lessonNinePartThree } from "./part_3";
+import { lessonNinePartFour } from "./part_4";
+import { lessonNinePartFive } from "./part_5";
+import { lessonNinePartSix } from "./part_6";
+import { lessonNineClosing } from "./closing";
+
+export const lessonNinePages = [
+  lessonNineOpening,
+  lessonNinePartOne,
+  lessonNinePartTwo,
+  lessonNinePartThree,
+  lessonNinePartFour,
+  lessonNinePartFive,
+  lessonNinePartSix,
+  lessonNineClosing
+];
